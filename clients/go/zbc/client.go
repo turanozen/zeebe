a5d66b89b4448c8e64a970ea6bb81f4693259597
Miguel Pires <miguel.pires@camunda.com>
2019-07-08 16:10:08 +0200

    feat(clients/go): added TLS in Go client

--- clients/go/zbc/client.go
+++ clients/go/zbc/client.go
@@ -16,90 +16,93 @@
 package zbc
 
 import (
-	"time"
+    "google.golang.org/grpc/credentials"
+    "time"
 
-	"github.com/zeebe-io/zeebe/clients/go/commands"
-	"github.com/zeebe-io/zeebe/clients/go/pb"
-	"github.com/zeebe-io/zeebe/clients/go/worker"
-	"google.golang.org/grpc"
+    "github.com/zeebe-io/zeebe/clients/go/commands"
+    "github.com/zeebe-io/zeebe/clients/go/pb"
+    "github.com/zeebe-io/zeebe/clients/go/worker"
+    "google.golang.org/grpc"
 )
 
 const DefaultRequestTimeout = 15 * time.Second
 
 type ZBClientImpl struct {
-	gateway        pb.GatewayClient
-	requestTimeout time.Duration
-	connection     *grpc.ClientConn
+    gateway        pb.GatewayClient
+    requestTimeout time.Duration
+    connection     *grpc.ClientConn
 }
 
 func (client *ZBClientImpl) NewTopologyCommand() *commands.TopologyCommand {
-	return commands.NewTopologyCommand(client.gateway, client.requestTimeout)
+    return commands.NewTopologyCommand(client.gateway, client.requestTimeout)
 }
 
 func (client *ZBClientImpl) NewDeployWorkflowCommand() *commands.DeployCommand {
-	return commands.NewDeployCommand(client.gateway, client.requestTimeout)
+    return commands.NewDeployCommand(client.gateway, client.requestTimeout)
 }
 
 func (client *ZBClientImpl) NewPublishMessageCommand() commands.PublishMessageCommandStep1 {
-	return commands.NewPublishMessageCommand(client.gateway, client.requestTimeout)
+    return commands.NewPublishMessageCommand(client.gateway, client.requestTimeout)
 }
 
 func (client *ZBClientImpl) NewResolveIncidentCommand() commands.ResolveIncidentCommandStep1 {
-	return commands.NewResolveIncidentCommand(client.gateway, client.requestTimeout)
+    return commands.NewResolveIncidentCommand(client.gateway, client.requestTimeout)
 }
 
 func (client *ZBClientImpl) NewCreateInstanceCommand() commands.CreateInstanceCommandStep1 {
-	return commands.NewCreateInstanceCommand(client.gateway, client.requestTimeout)
+    return commands.NewCreateInstanceCommand(client.gateway, client.requestTimeout)
 }
 
 func (client *ZBClientImpl) NewCancelInstanceCommand() commands.CancelInstanceStep1 {
-	return commands.NewCancelInstanceCommand(client.gateway, client.requestTimeout)
+    return commands.NewCancelInstanceCommand(client.gateway, client.requestTimeout)
 }
 
 func (client *ZBClientImpl) NewCompleteJobCommand() commands.CompleteJobCommandStep1 {
-	return commands.NewCompleteJobCommand(client.gateway, client.requestTimeout)
+    return commands.NewCompleteJobCommand(client.gateway, client.requestTimeout)
 }
 
 func (client *ZBClientImpl) NewFailJobCommand() commands.FailJobCommandStep1 {
-	return commands.NewFailJobCommand(client.gateway, client.requestTimeout)
+    return commands.NewFailJobCommand(client.gateway, client.requestTimeout)
 }
 
 func (client *ZBClientImpl) NewUpdateJobRetriesCommand() commands.UpdateJobRetriesCommandStep1 {
-	return commands.NewUpdateJobRetriesCommand(client.gateway, client.requestTimeout)
+    return commands.NewUpdateJobRetriesCommand(client.gateway, client.requestTimeout)
 }
 
 func (client *ZBClientImpl) NewSetVariablesCommand() commands.SetVariablesCommandStep1 {
-	return commands.NewSetVariablesCommand(client.gateway, client.requestTimeout)
+    return commands.NewSetVariablesCommand(client.gateway, client.requestTimeout)
 }
 
 func (client *ZBClientImpl) NewActivateJobsCommand() commands.ActivateJobsCommandStep1 {
-	return commands.NewActivateJobsCommand(client.gateway, client.requestTimeout)
+    return commands.NewActivateJobsCommand(client.gateway, client.requestTimeout)
 }
 
 func (client *ZBClientImpl) NewJobWorker() worker.JobWorkerBuilderStep1 {
-	return worker.NewJobWorkerBuilder(client.gateway, client, client.requestTimeout)
+    return worker.NewJobWorkerBuilder(client.gateway, client, client.requestTimeout)
 }
 
 func (client *ZBClientImpl) SetRequestTimeout(requestTimeout time.Duration) ZBClient {
-	client.requestTimeout = requestTimeout
-	return client
+    client.requestTimeout = requestTimeout
+    return client
 }
 
 func (client *ZBClientImpl) Close() error {
-	return client.connection.Close()
+    return client.connection.Close()
 }
 
 func NewZBClient(gatewayAddress string) (ZBClient, error) {
-	var opts []grpc.DialOption
-	opts = append(opts, grpc.WithInsecure())
+    var opts []grpc.DialOption
 
-	conn, err := grpc.Dial(gatewayAddress, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return &ZBClientImpl{
-		gateway:        pb.NewGatewayClient(conn),
-		requestTimeout: DefaultRequestTimeout,
-		connection:     conn,
-	}, nil
+    creds, _ := credentials.NewClientTLSFromFile("../resources/ca.cert.pem", "zeebe-server")
+    opts = append(opts, grpc.WithTransportCredentials(creds))
+
+    conn, err := grpc.Dial(gatewayAddress, opts...)
+    if err != nil {
+        return nil, err
+    }
+    return &ZBClientImpl{
+        gateway:        pb.NewGatewayClient(conn),
+        requestTimeout: DefaultRequestTimeout,
+        connection:     conn,
+    }, nil
 }
